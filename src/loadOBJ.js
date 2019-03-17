365597da099a3aa713ff2018cbfe9e221730b30b
mmcquaig <mmcquaig@uncc.edu>
2019-03-17 01:02:48 -0400

    added loadOBJ class and jquary/ajax references

--- src/loadOBJ.js
+++ src/loadOBJ.js
@@ -3,3 +3,25 @@ File to handle loading in .obj models and their properties such as positions, no
 @created by Matthew McQuaigue 
 @date 3/16/2019
 */
+
+class LoadOBJ{
+    constructor(objFileName){
+        this.url = "objFileName";
+        this.textures = [];
+    }
+
+    function addTexture(textureFileName){
+        if(typeof textureFileName != 'string'){
+            console.error("Texture Filename must be a string");
+        }
+        this.textures.push(textureFileName);
+    }
+
+    function getLastTexture(){
+        return this.textures[this.textures.length-1];
+    }
+
+    function getAllTextures(){
+        return this.textures;
+    }
+}
