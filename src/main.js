ae4a321c5c4af7915ab5145d512c7c6dc59797ae
mmcquaig <mmcquaig@uncc.edu>
2019-03-17 23:30:57 -0400

    finished object loader that parses obj file extracting needed data and rendering

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ function main(){
 
     var pointsArray = [];
     var colorsArray = [];
+    var indexArray = [];
     var normalsArray = [];
     var texCoordsArray = [];
     var groupData = [];
@@ -16,8 +17,10 @@ function main(){
         new Vec2(1, 0)
     ];
 
-    var modelViewMatrix, projectionMatrix;
-    var modelViewMatrixLoc, projectionMatrixLoc;
+    var rotAngle = 90;
+
+    var viewMatrix, projectionMatrix, modelMatrix;
+    var modelViewMatrixLoc, projectionMatrixLoc, modelMatrixLoc;
 
     window.onload = function init(){
         canvas = document.getElementById("gl-canvas"); //get the canvas instance
@@ -32,32 +35,39 @@ function main(){
         program = initShaders(gl, "vertex-shader", "fragment-shader");
         gl.useProgram(program);
 
-        var house = new LoadOBJ("../objects/simple_house.obj");
+        var house = new LoadOBJ("../objects/cat.obj");
         var objurl = house.loadMeshDataTriangle();
         objurl = objurl.responseText // get the text from the ajax response
         
-        data = loadMeshData(objurl);
-
-        var nBuffer = gl.createBuffer();
-        gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
-        gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
-
-        var vNormal = gl.getAttribLocation( program, "vNormal" );
-        gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
-        gl.enableVertexAttribArray( vNormal );
+        data = ObjLoader.domToMesh("house", objurl, true);
+        pointsArray = data[1];
+        normalsArray = data[2];
+        indexArray = data[0];
 
         var vBufferId = gl.createBuffer();
         gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId );
         gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW )
         
         var vPosition = gl.getAttribLocation( program, "vPosition" );
-        gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+        gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
         gl.enableVertexAttribArray( vPosition );
 
-        modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+        var indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(flatten(indexArray)), gl.STATIC_DRAW);
+
+        modelMatrixLoc = gl.getUniformLocation(program, "modelMatrix");
+        modelViewMatrixLoc = gl.getUniformLocation( program, "viewMatrix" );
         projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
+        var nBufferId = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, nBufferId );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW )
 
+        var nPosition = gl.getAttribLocation( program, "nPosition" );
+        gl.vertexAttribPointer( nPosition, 3, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( nPosition );
+        
         render();
 
     }
@@ -65,76 +75,97 @@ function main(){
     function render(){
         gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        modelViewMatrix = new Mat4();
-        modelViewMatrix.lookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+        rotAngle += 0.1;
+
+        modelMatrix = new Mat4();
+        modelMatrix.translate([0.0,-8.0,-50.0])
+        modelMatrix.scale([0.5,0.5,0.5])
+        modelMatrix.rotate(90, 1.0, 0.0, 0.0)
+        modelMatrix.rotate(rotAngle, 0.0,0.0,1.0)
+
+        viewMatrix = new Mat4();
+        viewMatrix.lookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
         projectionMatrix = new Mat4();
-        projectionMatrix.setPerspective(90, 1, 1.0, 2000);
+        projectionMatrix.setPerspective(80, 1, 0.0001, 10);
 
-        gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-        gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+        gl.uniformMatrix4fv(modelViewMatrixLoc, false, viewMatrix.array);
+        gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix.array);
+        gl.uniformMatrix4fv(modelMatrixLoc, false, modelMatrix.array);
 
-        gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+        gl.drawElements(gl.TRIANGLES, indexArray.length, gl.UNSIGNED_SHORT, 0);
+        requestAnimationFrame(render)
     }
 
-    function loadMeshData(string) {
-        var lines = string.split("\n");
-        var positions = [];
-        var normals = [];
-        var vertices = [];
+    function flatten(v)
+    {
+        let n = v.length;
+        let elemsAreArrays = false;
 
-        for ( var i = 0 ; i < lines.length ; i++ ) {
-            var parts = lines[i].trimRight().split(' ');
-            if ( parts.length > 0 ) {
-                switch(parts[0]) {
-                    case 'v':  positions.push(
-                        vec3.fromValues(
-                            parseFloat(parts[1]),
-                            parseFloat(parts[2]),
-                            parseFloat(parts[3])
-                        ));
-                    break;
-                    case 'vn':
-                        normals.push(
-                            vec3.fromValues(
-                            parseFloat(parts[1]),
-                            parseFloat(parts[2]),
-                            parseFloat(parts[3])
-                        ));
-                        break;
-                        case 'f': {
-                            var f1 = parts[1].split('/');
-                            var f2 = parts[2].split('/');
-                            var f3 = parts[3].split('/');
-                            Array.prototype.push.apply(
-                                vertices, positions[parseInt(f1[0]) - 1]
-                            );
-                            Array.prototype.push.apply(
-                                vertices, normals[parseInt(f1[2]) - 1]
-                            );
-                            Array.prototype.push.apply(
-                                vertices, positions[parseInt(f2[0]) - 1]
-                            );
-                            Array.prototype.push.apply(
-                                vertices, normals[parseInt(f2[2]) - 1]
-                            );
-                            Array.prototype.push.apply(
-                                vertices, positions[parseInt(f3[0]) - 1]
-                            );
-                            Array.prototype.push.apply(
-                                vertices, normals[parseInt(f3[2]) - 1]
-                            );
-                        break;
-                        }
-                    }
+        if (Array.isArray(v[0])) {
+            elemsAreArrays = true;
+            n *= v[0].length;
+        }
+
+        var floats = new Float32Array(n);
+
+        if (elemsAreArrays) {
+            let idx = 0;
+            for (let i = 0; i < v.length; ++i) {
+                for (let j = 0; j < v[i].length; ++j) {
+                    floats[idx++] = v[i][j];
                 }
             }
-            var vertexCount = vertices.length / 6;
-            console.log("Loaded mesh with " + vertexCount + " vertices");
-            return {
-                primitiveType: 'TRIANGLES',
-                vertices: new Float32Array(vertices),
-                vertexCount: vertexCount
-            };
+            }
+        else {
+            for (let i = 0; i < v.length; ++i) {
+                floats[i] = v[i];
+            }
+        }
+
+        return floats;
     }
+
+    function normalize( u, excludeLastComponent )
+    { 
+        if ( excludeLastComponent ) {
+            var last = u.pop();
+        }
+    
+        var len = length( u );
+
+        if ( !isFinite(len) ) {
+            throw "normalize: vector " + u + " has zero length";
+        }
+    
+        for ( var i = 0; i < u.length; ++i ) {
+            u[i] /= len;
+        }
+
+        if ( excludeLastComponent ) {
+            u.push( last );
+        }
+            
+        return u;
+    }
+
+    function length( u )
+    {
+        return Math.sqrt( dot(u, u) );
+    }
+
+    function dot( u, v )
+    {
+        if ( u.length != v.length ) {
+            throw "dot(): vectors are not the same dimension";
+        }
+
+        var sum = 0.0;
+        for ( var i = 0; i < u.length; ++i ) {
+            sum += u[i] * v[i];
+        }
+
+        return sum;
+    }
+
 }
