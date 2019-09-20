4a0c524f2157a5cff7e624ebcf4bd862f19fb8f2
quaiquai <mmcquaig@uncc.edu>
2019-09-20 12:38:31 -0400

    added particle count and color capabilites, fixed fading time

--- src/particle.js
+++ src/particle.js
@@ -3,7 +3,7 @@ class Particle{
   // (in degrees) and a speed (in pixels per second) and converts these values into a velocity vector.
   // This is intentional, when setting a particle up it’s easier to work with an angle and a speed.
   // But when updating the particle each frame, it’s easier to work with a velocity vector.
-  constructor(x, y, l, angle, speed){
+  constructor(x, y, l, angle, speed, color){
     this.position = {
       x: x,
       y: y
@@ -13,15 +13,16 @@ class Particle{
       x: (speed * Math.cos(angleInRad)),
       y: -(speed * Math.sin(angleInRad))
     }
-    this.color = 'blue';
     this.lifeTime = l;
+    this.color = color;
   }
 
   update(dt){
     this.lifeTime -= dt;
     if(this.lifeTime > 0){
-      this.position.x += this.velocity.x * dt;
-      this.position.y += this.velocity.y * dt;
+      this.position.x += this.velocity.x * dt * 0.03;
+      this.position.y += this.velocity.y * dt * 0.03;
+      this.color[3] = this.lifeTime;
     }
   }
 }
--- src/particleStream.js
+++ src/particleStream.js
@@ -1,10 +1,11 @@
 class ParticleStream{
-  constructor(x, y, l, angle, speed, count){
+  constructor(x, y, l, count){
     this.particleCount = count;
     this.particleList = [];
     this.particleStreamVerts = [];
     for (let i = 0; i < count; i++){
-      this.particleList.push(new Particle(x, y, l, angle, speed));
+      this.particleList.push(new Particle(x, y, l, Math.random() * 360, Math.random() * 20,
+                                          [Math.random(), Math.random(), Math.random(), 1.0]));
       this.particleStreamVerts.push(x, y);
     }
   }
