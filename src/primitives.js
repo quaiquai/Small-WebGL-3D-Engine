05bb1302be317dbed9a63ce3882bf7b15904811c
mmcquaig <mmcquaig@uncc.edu>
2019-12-23 17:42:31 -0500

    added basic directional lighting

--- src/primitives.js
+++ src/primitives.js
@@ -24,9 +24,9 @@ class Cube extends Primitives{
 
       // Top face
       -sz,  sz, -sz,
-      -sz,  sz,  sz,
-       sz,  sz,  sz,
-       sz,  sz, -sz,
+      sz,  sz,  -sz,
+       -sz,  sz,  sz,
+       sz,  sz, sz,
 
       // Bottom face
       -sz, 0, -sz,
@@ -46,6 +46,43 @@ class Cube extends Primitives{
       -sz,  sz,  -sz,
       -sz,  sz, sz
     ]
+    this.normals = [
+      //Front face
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+
+      //Back face
+      0, 0, -1,
+      0, 0, -1,
+      0, 0, -1,
+      0, 0, -1,
+
+      //Top face
+      0, 1, 0,
+      0, 1, 0,
+      0, 1, 0,
+      0, 1, 0,
+
+      //Bottom face
+      0, -1, 0,
+      0, -1, 0,
+      0, -1, 0,
+      0, -1, 0,
+
+      //Right face
+      1, 0, 0,
+      1, 0, 0,
+      1, 0, 0,
+      1, 0, 0,
+
+      //left face
+      -1, 0, 0,
+      -1, 0, 0,
+      -1, 0, 0,
+      -1, 0, 0
+    ];
     this.color = [
       Math.random(),  Math.random(),  Math.random(),  1.0,    // Front face: white
     ];
@@ -58,6 +95,11 @@ class Cube extends Primitives{
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    this.nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.normals), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
   }
 
   associateBuffers(){
@@ -65,15 +107,22 @@ class Cube extends Primitives{
     var coord = gl.getAttribLocation(program, "coordinates");
     gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coord);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.nBuffer);
+    var norms = gl.getAttribLocation(program, "a_normal");
+    gl.vertexAttribPointer(norms, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(norms);
   }
 
   genUniforms(){
     this.u_model = gl.getUniformLocation(program, "u_model");
     this.u_color = gl.getUniformLocation(program, "u_color");
+    this.reverseLightDirectionLocation = gl.getUniformLocation(program, "u_reverseLightDirection");
   }
 
   setUniforms(){
     gl.uniformMatrix4fv(this.u_model, false, new Mat4().array);
     gl.uniform4fv(this.u_color, this.color);
+    gl.uniform3fv(this.reverseLightDirectionLocation, normalize([0.0, 0.7, 1.0]));
   }
 }
