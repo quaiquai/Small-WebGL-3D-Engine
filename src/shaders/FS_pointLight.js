c23f79d3843ca7430fe611e48fe0fe1e5190b8b9
quaiquai <mmcquaig@uncc.edu>
2020-01-01 00:25:13 -0500

    added specular and light color

--- src/shaders/FS_pointLight.js
+++ src/shaders/FS_pointLight.js
@@ -2,8 +2,12 @@ var FS_pointLighting = `
 	precision mediump float;
 	varying vec3 v_normal;
 	varying vec3 v_surfaceToLight;
+	varying vec3 v_surfaceToView;
 
 	uniform vec4 u_color;
+	uniform vec3 u_lightColor;
+	uniform vec3 u_specularColor;
+	uniform float u_shininess;
 
 	void main(){
   		// because v_normal is a varying it's interpolated
@@ -12,11 +16,22 @@ var FS_pointLighting = `
   		vec3 normal = normalize(v_normal);
 
   		vec3 surfaceToLightDirection = normalize(v_surfaceToLight);
+  		vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+  		vec3 halfVector = normalize(surfaceToLightDirection + surfaceToViewDirection);
 
   		float light = dot(normal, surfaceToLightDirection);
 
+  		float specular = 0.0;
+  		if(light > 0.0){
+  			specular = pow(dot(normal, halfVector), u_shininess);
+  		}
+
   		gl_FragColor = u_color;
+
   		// Lets multiply just the color portion (not the alpha)
   		// by the light
-  		gl_FragColor.rgb *= light;
+  		gl_FragColor.rgb *= light * u_lightColor;
+
+  		// Just add in the specular
+  		gl_FragColor.rgb += specular * u_specularColor;
 	}`;
--- src/shaders/VS_pointLight.js
+++ src/shaders/VS_pointLight.js
@@ -6,9 +6,11 @@ var VS_pointLighting = `
 	uniform mat4 u_view;
 	uniform mat4 u_projection;
 	uniform vec3 u_lightPosition;
+	uniform vec3 u_viewPosition;
 
 	varying vec3 v_normal;
 	varying vec3 v_surfaceToLight;
+	varying vec3 v_surfaceToView;
 
 	void main(){
 		vec4 position = vec4(u_projection * u_view * u_model * vec4(coordinates, 1.0));
@@ -17,5 +19,6 @@ var VS_pointLighting = `
 		vec3 surfaceWorldPosition = (u_model * vec4(coordinates, 1.0)).xyz; //position of the primitives surface
 		v_normal = mat3(u_model) * a_normal;
 		v_surfaceToLight = u_lightPosition - surfaceWorldPosition; //direction vector from light to surface of primitive
+		v_surfaceToView = u_viewPosition - surfaceWorldPosition; //direction vector from view to surface of primitive
 	}`;
 
